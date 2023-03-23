c8c822fa6c8110992d4fc02b43c6c2170a52b41a
beligrous <beligrous@gmail.com>
2023-03-23 21:18:50 +0200

    add fs func listContacts and exports

---
+++ contacts.js
@@ -0,0 +1,27 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
+
+module.exports = { listContacts, removeContact, getContactById, addContact };
---
+++ index.js
@@ -0,0 +1,3 @@
+const { listContacts } = require("./contacts");
+
+listContacts();
