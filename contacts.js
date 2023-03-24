eeea7bb5715287c4502264c7a26e96300c2f809a
beligrous <beligrous@gmail.com>
2023-03-24 12:40:21 +0200

    add crud functions

--- contacts.js
+++ contacts.js
@@ -1,27 +1,49 @@
 const fs = require("fs").promises;
 const path = require("path");
+const { nanoid } = require("nanoid");
 
 const contactsPath = path.resolve("./db/contacts.json");
 
 async function listContacts() {
   try {
-    const data = await fs.readFile(contactsPath, "utf8");
-    console.log(data);
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
   } catch (error) {
-    console.error(error);
+    return error;
   }
 }
 
-function getContactById(contactId) {
-  // ...твій код
+async function getContactById(contactId) {
+  const data = await listContacts();
+  const contactIndex = data.findIndex((item) => item.id === contactId);
+  if (contactIndex === -1) {
+    console.log("There is no contact with such id !");
+  }
+  return data[contactIndex];
 }
 
-function removeContact(contactId) {
-  // ...твій код
+async function removeContact(contactId) {
+  const data = await listContacts();
+  const contactIndex = data.findIndex((item) => item.id === contactId);
+  if (contactIndex === -1) {
+    console.log("There is no contact with such id !");
+  }
+  const [result] = data.splice(contactIndex, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 1));
+  return result;
 }
 
-function addContact(name, email, phone) {
-  // ...твій код
+async function addContact(name, email, phone) {
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  const data = await listContacts();
+  data.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 1));
+  return newContact;
 }
 
 module.exports = { listContacts, removeContact, getContactById, addContact };
--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const { listContacts } = require("./contacts");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
 
-listContacts();
+const result = async () => {
+  //   const list = await listContacts();
+  //   console.log(list);
+  //   const byId = await getContactById("qdggE76Jtbfd9eWJHrssH");
+  //   console.log(byId);
+  // const remove = await removeContact("qdggE76Jtbfd9eWJHrssH");
+  // console.log(remove);
+  // const add = await addContact(
+  //   "Chaim Lewis",
+  //   "dui.in@egetlacus.ca",
+  //   "(294) 840-6685"
+  // );
+  // console.log(add);
+};
+
+result();
