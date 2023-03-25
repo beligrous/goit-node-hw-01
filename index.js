69c8d63c4754c2752da6792a3499eb75e2d0e532
beligrous <beligrous@gmail.com>
2023-03-25 08:36:37 +0200

    fix console.table

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const list = await listContacts();
-      console.log(list);
+      console.table(list);
       break;
 
     case "get":
